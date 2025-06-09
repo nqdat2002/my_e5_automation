21c74a23fa702c63af6e2524d4ca515d396baffd
Nguyễn Quốc Đạt <datfeed.2706@gmail.com>
2025-06-09 23:20:27 +0700

    update some files config

---
+++ modules/create_flow.py
@@ -0,0 +1,13 @@
+import requests
+
+def create_flow(pat_token, org, project, repo_name):
+    url = f"https://dev.azure.com/{org}/{project}/_apis/git/repositories?api-version=6.0"
+    headers = {
+        "Authorization": f"Basic {pat_token}",
+        "Content-Type": "application/json"
+    }
+    body = {
+        "name": repo_name
+    }
+    response = requests.post(url, headers=headers, json=body)
+    print(response.status_code, response.json())
